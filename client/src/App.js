1bf81f3971c00d68695a4ca81252ba1c39eeb82c
Sergey Batenin <inferno.87@mail.ru>
2023-09-10 02:16:50 +0300

    Добавлена кнопка для добавления нового контакта

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import React, {useState} from 'react';
 import TableComp from './blocks/TableComp';
 
 function App() {
 
-  const contacts = [
-    {id: 1, fullName: " FIO 1", phone: "+7 123 456 78 91", note: "note 1"},
-    {id: 2, fullName: " FIO 2", phone: "+7 123 456 78 92", note: "note 2"},
-    {id: 3, fullName: " FIO 3", phone: "+7 123 456 78 93", note: "note 3"}
-  ]
+  const [contacts, setContacts] = useState(
+    [
+      {id: 1, fullName: " FIO 1", phone: "+7 123 456 78 91", note: "note 1"},
+      {id: 2, fullName: " FIO 2", phone: "+7 123 456 78 92", note: "note 2"},
+      {id: 3, fullName: " FIO 3", phone: "+7 123 456 78 93", note: "note 3"}
+    ]
+  )
+
+  const appendContact = () => {    
+    const newContact = {id: 7, fullName: " FIO 7", phone: "+7 123 456 78 97", note: "note 7"};
+    setContacts([...contacts, newContact]);
+  }
 
   return (
     <div className='container mt-5'>
@@ -17,7 +25,15 @@ function App() {
         </div>
         <div className='card-body'>
           <TableComp data={contacts} />
+          <div>
+          <button
+            type='button'
+            className='btn btn-primary'
+            onClick={appendContact}>
+            Добавить контакт
+          </button>
         </div>
+        </div>                
       </div>
     </div>
   );
